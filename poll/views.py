cc3c07c6e4713b61f907171afd6bd7f5651241c3
Abrajam7611 <luis.vel4x@gmail.com>
2024-05-20 13:59:41 -0600

    Tercer cambio

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from.models import Question
 # Create your views here.
+def home(request):
+    question = Question.objects.all()
+    return render(
+        request,
+        'poll/home.html',
+        {
+            "questions": question
+        })
+
+
+def vote(request, q_id):
+    q = Question.objects.get(pk=q_id)
+    return render(request, 'poll/vote.html',{
+        "question": q
+    })
+
+def result(request, q_id):
+    pass
